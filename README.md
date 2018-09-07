561ae3a08e55301b550b3aa5264470c7bfde062f
Kyle Lynch <kylelynch104@gmail.com>
2018-09-06 17:24:24 -0700

    version_1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+#  Song Lyrics Finder
+
+###  A simple web app that searches for popular song lyrics.  Built with Javascript, React, Context API, and Axios.  Also using Bootstrap and Font Awesome.  
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-8 h1 mx-auto">Lyric Finder</span>
+    </div>
+  );
+};
+
+export default Navbar;

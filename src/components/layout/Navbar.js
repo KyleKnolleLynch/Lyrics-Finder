4be135897a2de1fdbd455ef8d53af2317be70cca
Kyle Lynch <kylelynch104@gmail.com>
2018-09-06 18:03:28 -0700

    version_1.0.1

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className="navbar navbar-dark bg-dark mb-5">
-      <span className="navbar-brand mb-8 h1 mx-auto">Lyric Finder</span>
+      <span className="navbar-brand mb-8 h1 mx-auto">Lyric Searcher</span>
     </div>
   );
 };
--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -9,7 +9,7 @@ class Search extends Component {
 
   findTrack = (dispatch,e) => {
     e.preventDefault();
-    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&apikey=${process.env.REACT_APP_MM_KEY}`)
+    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
 
     .then(res => {
         dispatch({
